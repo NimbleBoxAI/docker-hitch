dc3f8b640142986ecb6faf90a1f92c6a477ceb6c
override80 <43748388+override80@users.noreply.github.com>
2021-04-09 09:22:13 +0200

    feat: initial commit

--- start.sh
+++ start.sh
@@ -12,20 +12,16 @@ if [[ -f $HITCH_KEY && -f $HITCH_CERT ]] && [[ ! ( -z $HITCH_PEM && -f $HITCH_PE
 elif [ -f $HITCH_PEM ]; then
   echo Using $HITCH_PEM
 else
-  # check if $DOMAIN is set, otherwise set to example.com
-  if [ -z $DOMAIN ]; then
-      export DOMAIN="example.com"
-  fi  
-
-  echo "Couldn't find PEM file, creating one for domain $DOMAIN"
+  echo "Couldn't find PEM file, creating one for domain $IP"
   cd /etc/ssl/hitch
-  openssl req -newkey rsa:2048 -sha256 -keyout example.com.key -nodes -x509 -days 365 -out example.crt -subj "/C=CH/ST=Zurich/L=Zurich/O=Snakeoil Inc/OU=IT Department/CN=$DOMAIN"
+  # please replace X.X.X.X with external IP and public.dns.name with external FQDN
+  openssl req -newkey rsa:2048 -sha256 -keyout example.com.key -nodes -x509 -days 36500 -out example.crt -subj "/C=IT/ST=Neverland/L=WholeWorld/O=Mycompany/OU=SRE/CN=X.X.X.X" -addext "subjectAltName=IP.1:$MY_POD_IP,IP.2:X.X.X.X,IP.3:127.0.0.1,DNS.1:public.dns.name"
   cat example.com.key example.crt > combined.pem
 fi
-
 
 exec bash -c \
   "exec /usr/local/sbin/hitch --user=hitch \
   $HITCH_PARAMS \
   --ciphers=$HITCH_CIPHER \
   $HITCH_PEM"
+
