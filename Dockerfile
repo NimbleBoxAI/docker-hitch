7f66d99bcc52592d1ef138bc3b19e0597d9d5d17
Shubham Rao <shubham@nimblebox.ai>
2022-02-19 03:43:06 +0530

    feat: adapt for nimblebox test-2 ingress

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM alpine:3.12
-MAINTAINER Adrian Gschwend <adrian.gschwend@zazuko.com>
+LABEL MAINTAINER Adrian Gschwend <adrian.gschwend@zazuko.com>
 
 # Only update below
-ARG HITCH_VERSION=1.7.0
+ARG HITCH_VERSION=1.7.2
 
 # dependencies
 RUN apk --update add bash build-base libev libev-dev automake openssl openssl-dev autoconf curl byacc flex
 # get & build
-RUN cd /tmp && curl -L https://hitch-tls.org/source/hitch-${HITCH_VERSION}.tar.gz | tar xz 
+RUN cd /tmp && curl -L https://hitch-tls.org/source/hitch-${HITCH_VERSION}.tar.gz | tar xz
 RUN cd /tmp/hitch* && ./configure --with-rst2man=/bin/true
 RUN cd /tmp/hitch* && make && make install
 RUN mkdir -p /etc/ssl/hitch
@@ -23,7 +23,7 @@ ADD start.sh /start.sh
 
 ENV HITCH_PEM    /etc/ssl/hitch/combined.pem
 # pelase set backend port to your GRCP container one and frontend to the one you want to expose
-ENV HITCH_PARAMS "--backend=[127.0.0.1]:8000 --frontend=[*]:8443+/etc/ssl/hitch/combined.pem"
+ENV HITCH_PARAMS "--backend=[127.0.0.1]:50051 --frontend=[*]:8443+/etc/ssl/hitch/combined.pem"
 ENV HITCH_CIPHER ECDHE-RSA-AES128-GCM-SHA256
 
 CMD /start.sh
--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 set -e
 
 # check if we have a PEM file or it's two components, otherwise generate a self-signed one
-if [[ -f $HITCH_KEY && -f $HITCH_CERT ]] && [[ ! ( -z $HITCH_PEM && -f $HITCH_PEM) ]]; then
+if [[ -f $HITCH_KEY && -f $HITCH_CERT ]] && [[ ! (-z $HITCH_PEM && -f $HITCH_PEM) ]]; then
   HITCH_PEM=/etc/ssl/hitch/combined.pem
   touch $HITCH_PEM
   chmod 440 $HITCH_PEM
-  cat $HITCH_KEY $HITCH_CERT > $HITCH_PEM
+  cat $HITCH_KEY $HITCH_CERT >$HITCH_PEM
   echo Combined $HITCH_KEY and $HITCH_CERT
 elif [ -f $HITCH_PEM ]; then
   echo Using $HITCH_PEM
@@ -15,8 +15,8 @@ else
   echo "Couldn't find PEM file, creating one for domain $IP"
   cd /etc/ssl/hitch
   # please replace X.X.X.X with external IP and public.dns.name with external FQDN
-  openssl req -newkey rsa:2048 -sha256 -keyout example.com.key -nodes -x509 -days 36500 -out example.crt -subj "/C=IT/ST=Neverland/L=WholeWorld/O=Mycompany/OU=SRE/CN=X.X.X.X" -addext "subjectAltName=IP.1:$MY_POD_IP,IP.2:X.X.X.X,IP.3:127.0.0.1,DNS.1:public.dns.name"
-  cat example.com.key example.crt > combined.pem
+  openssl req -newkey rsa:2048 -sha256 -keyout example.com.key -nodes -x509 -days 36500 -out example.crt -subj "/C=IN/ST=Neverland/L=WholeWorld/O=Nimblebox/OU=SRE/CN=34.117.69.179" -addext "subjectAltName=IP.1:$MY_POD_IP,IP.2:34.117.69.179,IP.3:127.0.0.1,DNS.1:grpc.revamp-online.test-2.nimblebox.ai"
+  cat example.com.key example.crt >combined.pem
 fi
 
 exec bash -c \
@@ -26,4 +26,3 @@ exec bash -c \
   --tls-protos="TLSv1.2" \
   --alpn-protos="h2" \
   $HITCH_PEM"
-
